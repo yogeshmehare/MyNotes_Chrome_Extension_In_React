9f87883cd99e9cb856cda180a17103c40ef983ec
Yogesh Mehare <yogeshmehare124@gmail.com>
2024-01-20 22:50:39 +0530

    UI textfields added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import TextField from "@mui/material/TextField";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import Button from "@mui/material/Button";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import Stack from "@mui/material/Stack";
 
 function App() {
   //variables
@@ -7,33 +13,86 @@ function App() {
   const [content, setContent] = useState("");
   const [showInputFields, setshowInputFields] = useState(false);
 
+  function makeNoteFeildsEmpty() {
+    setTitle("")
+    setContent("")
+  }
+
   return (
     <div className="App">
       <div className="container">
         <h1>Hio</h1>
-        <button
-          id="createNoteButton"
-          onClick={(event: React.MouseEvent<HTMLElement>) =>
-            setshowInputFields(true)
-          }
-        >
-          Create new note
-        </button>
 
+        {!showInputFields && (
+          <Button
+            variant="contained"
+            id="createNoteButton"
+            color="primary"
+            onClick={(event: React.MouseEvent<HTMLElement>) =>
+              setshowInputFields(true)
+            }
+          >
+            Create new note
+          </Button>
+        )}
         {showInputFields && (
           <div>
-            <input
-              type="text"
+            <TextField
+              id="title"
+              label="Title"
+              variant="outlined"
               placeholder="Enter Title for Note"
+              margin="dense"
+              fullWidth
+              style={{"marginRight":"20px","paddingLeft":"10px"}}
               value={title}
+              color={title.length > 0 ? "success" : "secondary"}
               onChange={(e) => setTitle(e.target.value)}
-            ></input>
-            <input
-              type="text"
-              placeholder="Enter Title for Note"
+            />
+            <TextField
+              id="note"
+              label="Note"
+              multiline
+              rows={4}
+              defaultValue="Default Value"
+              placeholder="Enter Note"
+              margin="dense"
+              fullWidth
+              style={{"marginRight":"20px","paddingLeft":"10px"}}
+              color={content.length > 0 ? "success" : "secondary"}
               value={content}
               onChange={(e) => setContent(e.target.value)}
-            ></input>
+            />
+            <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+              <Button
+                variant="contained"
+                color="primary"
+                endIcon={<CheckCircleIcon color="success" />}
+                id="createNoteButton"
+                onClick={(event: React.MouseEvent<HTMLElement>) =>
+                  {
+                    setshowInputFields(false)
+                    makeNoteFeildsEmpty()
+                  }
+                }
+              >
+                Create
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                endIcon={<DeleteIcon color="error" />}
+                id="closeNoteButton"
+                onClick={(event: React.MouseEvent<HTMLElement>) =>
+                  {
+                    setshowInputFields(false);
+                    makeNoteFeildsEmpty()
+                  }
+                }
+              >
+                Close
+              </Button>
+            </Stack>
           </div>
         )}
       </div>
@@ -42,3 +101,4 @@ function App() {
 }
 
 export default App;
+
